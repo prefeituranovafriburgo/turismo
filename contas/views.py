f529ff3d69a2d4e39d75850bfbe8f4e3c6e14f6a
Luis Eduardo Cordeiro Salarini <eduardo.pmnf@gmail.com>
2022-03-10 17:20:41 -0300

    change reset email

--- contas/views.py
+++ contas/views.py
@@ -265,7 +265,7 @@ def password_reset_request(request):
 					}
 					email = render_to_string(email_template_name, c)
 					try:
-						send_mail(subject, email, [user.email] , [user.email], fail_silently=False)
+						send_mail(subject, email, user.email, [user.email], fail_silently=False)
 					except BadHeaderError:
 						return HttpResponse('Invalid header found.')
 					return redirect ("password_reset_done")
