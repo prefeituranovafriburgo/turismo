e893b4c8629466dca6ceec4f134e987069c44669
Luis Eduardo Cordeiro Salarini <eduardo.pmnf@gmail.com>
2022-03-29 17:12:45 -0300

    'hotel que' para 'hotel onde'

--- turismo/envvars.py
+++ turismo/envvars.py
@@ -7,7 +7,8 @@ except ImportError:
 
 def load_envars(BASE_DIR):
     try:
-        yaml_file=open(str(BASE_DIR.parent) + "/.envvars.yaml", "r")
+        yaml_file=open("./.envvars.yaml", "r")        
     except:
-        yaml_file=open("./.envvars.yaml", "r")
+        yaml_file=open(str(BASE_DIR.parent) + "/.envvars.yaml", "r")
+        
     return yaml.load(yaml_file, Loader=Loader)
