ecb178b839bca28f8d9705897009a7e0aba79c96
Luis Eduardo Cordeiro Salarini <eduardo.pmnf@gmail.com>
2022-04-25 16:01:31 -0300

    feat: mudança na kpi -> filtro em viavem v1.1.0

--- KPIs/urls.py
+++ KPIs/urls.py
@@ -7,4 +7,6 @@ app_name='KPIs'
 
 urlpatterns = [
     path('', views.index, name='main'),    
+    path('viagens', views.viagens, name='viagens'),  
+    path('usuarios', views.usuarios, name='usuarios'), 
 ]
